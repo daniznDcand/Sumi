8b8dbd217cfca58b0619f74b16ccbbd57ff8d911
Ell Sarmiento 🦩 <sarmientodieg@gmail.com>
2026-01-05 19:45:43 -0500

    Actualizar chatgpt.js

--- commands/ai/chatgpt.js
+++ commands/ai/chatgpt.js
@@ -5,15 +5,6 @@ export default {
   category: 'ai',
   run: async (client, m, args, command) => {
 
-    const botId = client.user.id.split(':')[0] + '@s.whatsapp.net'
-    const isOficialBot = botId === global.client.user.id.split(':')[0] + '@s.whatsapp.net'
-    const isPremiumBot = global.db.data.settings[botId]?.botprem === true
-    const isModBot = global.db.data.settings[botId]?.botmod === true
-
-    if (!isOficialBot && !isPremiumBot && !isModBot) {
-      return client.reply(m.chat, `🌽 El comando *${command}* no esta disponible en *Sub-Bots.*`, m)
-    }
-
     const text = args.join(' ').toLowerCase()
 
     if (!text) {
