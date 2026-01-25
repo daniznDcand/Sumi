e8349a032ecc4f6757a2334363c32138927d0954
Ell Sarmiento 🦩 <sarmientodieg@gmail.com>
2026-01-25 14:08:10 -0500

    Actualizar play2.js

--- commands/dow/play2.js
+++ commands/dow/play2.js
@@ -2,27 +2,25 @@ import yts from 'yt-search'
 import fetch from 'node-fetch'
 import { getBuffer } from '../../lib/message.js'
 
-const isYTUrl = (url) => /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/i.test(url)
-
 export default {
   command: ['play2', 'mp4', 'ytmp4', 'ytvideo', 'playvideo'],
   category: 'downloader',
   run: async (client, m, args) => {
     try {
       if (!args[0]) {
-        return m.reply('🫛 Por favor, menciona el nombre o URL del video que deseas descargar')
+        return m.reply('🌵 Por favor, menciona el nombre o URL del video que deseas descargar')
       }
 
       const query = args.join(' ')
-      let url, title, videoInfo
+      let url, title
 
-      if (!isYTUrl(query)) {
+      if (!/^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\//i.test(query)) {
         const search = await yts(query)
         if (!search.all.length) {
-          return m.reply('🍋‍🟩 No se *encontraron* resultados')
+          return m.reply('🌵 No se *encontraron* resultados')
         }
 
-        videoInfo = search.all[0]
+        const videoInfo = search.all[0]
         url = videoInfo.url
         title = videoInfo.title
 
@@ -45,28 +43,30 @@ export default {
         })
       } else {
         url = query
-        title = 'YouTube Video'
       }
 
-      const res = await fetch(`${api.url2}/download/ytmp4?url=${encodeURIComponent(url)}&q=&api_key=${api.key}`)
+      const res = await fetch(`${api.url}/dl/ytmp4?url=${encodeURIComponent(url)}&quality=144&key=${api.key}`)
       const result = await res.json()
 
-      if (!result.status || !result.result?.url) {
-        return m.reply('🫛 No se pudo descargar el *video*, intenta más tarde.')
+      if (!result.data || !result.data.dl) {
+        return m.reply('🌵 No se pudo descargar el *video*, intenta más tarde.')
       }
 
-      const videoBuffer = await getBuffer(result.result.url)
+      const videoTitle = result.data.title
+      const dlUrl = result.data.dl
+
+      const videoRes = await fetch(dlUrl)
+      const videoBuffer = await videoRes.buffer()
 
       const mensaje = {
         video: videoBuffer,
-        fileName: result.result.filename || `${title}.mp4`,
+        fileName: `${videoTitle}.mp4`,
         mimetype: 'video/mp4'
       }
 
       await client.sendMessage(m.chat, mensaje, { quoted: m })
-
     } catch (e) {
-     // console.error(e)
+      console.log(e)
       await m.reply(msgglobal)
     }
   }
